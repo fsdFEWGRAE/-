aaba52932026ba483e6a3bde9c398299193f3d33
fsdFEWGRAE <17sutef3@gmail.com>
2025-12-07 19:20:05 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>GLOM Authorization</title>
+    <link rel="stylesheet" href="/glom.css">
+</head>
+<body class="landing-bg">
+
+<div class="intro-container">
+    <div class="logo-box">
+        <h1>GLOM Authorization</h1>
+    </div>
+
+    <div class="buttons">
+        <a href="/login" class="btn-login">Login</a>
+        <a href="https://discord.gg/Y9aE4YyCaS" target="_blank" class="btn-discord">Join Discord</a>
+    </div>
+</div>
+
+</body>
+</html>
